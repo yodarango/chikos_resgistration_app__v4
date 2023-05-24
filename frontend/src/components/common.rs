1c6badcacf54923b4d21a6729caba7e16ad7fa95
Daniel Rangel <danranngel@gmail.com>
2023-05-23 20:11:58 -0400

    added a few components

--- frontend/src/components/common.rs
+++ frontend/src/components/common.rs
@@ -1,14 +1,14 @@
 use yew::prelude::*;
 
 #[derive(PartialEq, Properties)]
-pub struct Props {
+pub struct ProfilePhotoProps {
     pub src: String,
     pub alt: Option<String>,
     pub class_name: Option<String>
 }
 
 #[function_component]
-pub fn ProfilePhoto(props: &Props) -> Html{
+pub fn ProfilePhoto(props: &ProfilePhotoProps) -> Html{
 
     let class_name: String = match &props.class_name {
         Some(string)=> String::from(string),
@@ -24,6 +24,31 @@ pub fn ProfilePhoto(props: &Props) -> Html{
     html!{
         <div class={class_name}>
             <img src={props.src.clone()} alt={alt}/>
+        </div>
+    }
+}
+
+// --------------------------------------------------------------
+#[derive(PartialEq, Properties)]
+pub struct InputProps {
+    input_type: Option<String>,
+    class_name: Option<String>,
+    placeholder: Option<String>,
+    value: Option<String>,
+}
+
+#[function_component]
+pub fn Input(props: &InputProps) -> Html{
+
+    let class_name = props.class_name.as_ref().unwrap_or(&String::from("")).clone();
+    let input_type = props.input_type.as_ref().unwrap_or(&String::from("text")).clone();
+    let placeholder = props.placeholder.as_ref().unwrap_or(&String::from("")).clone();
+    let value = props.value.as_ref().unwrap_or(&String::from("")).clone();
+
+
+    html!{
+        <div class={class_name}>
+            <input type={input_type} placeholder={placeholder} value={value}/>
         </div>
     }
 }
--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,7 +1,7 @@
 use yew::prelude::*;
 mod components;
 
-use components::common::ProfilePhoto;
+use components::common::{ProfilePhoto, Input};
 
 
 #[function_component(App)]
@@ -9,7 +9,10 @@ fn app() -> Html {
 
 
     html! {
-        <ProfilePhoto src="https://images.unsplash.com/photo-1682695795798-1b31ea040caf?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3270&q=80"/>
+        <div>
+            <ProfilePhoto src="https://images.unsplash.com/photo-1682695795798-1b31ea040caf?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3270&q=80"/>
+            <Input />
+        </div>
     }
 }
 
